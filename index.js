c102d0ec6580c8662734429558044e8f391c17d5
Punit Juneja <punitjuneja123@gmail.com>
2023-11-29 02:55:27 +0530

    check

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-// const port = 3000;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-const { uploadRouter } = require("./routes/upload.route");
+// const { uploadRouter } = require("./routes/upload.route");
 
 // basic route
 app.get("/", (req, res) => {
   res.send("Hello :)");
 });
 
-app.use(uploadRouter);
+// app.use(uploadRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`port is rocking at ${port}`);
--- routes/upload.route.js
+++ routes/upload.route.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const multer = require("multer");
-const fs = require("fs");
-// const path = require("path");
+// const express = require("express");
+// const multer = require("multer");
+// const fs = require("fs");
+// // const path = require("path");
 
-const uploadRouter = express.Router();
+// const uploadRouter = express.Router();
 
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: "uploads",
-  }),
-});
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: "uploads",
+//   }),
+// });
 
-uploadRouter.post("/upload", upload.single("file"), (req, res) => {
-  const file = req.file;
-  const stream = fs.createWriteStream(`uploads/${file.originalname}`);
+// uploadRouter.post("/upload", upload.single("file"), (req, res) => {
+//   const file = req.file;
+//   const stream = fs.createWriteStream(`uploads/${file.originalname}`);
 
-  fs.createReadStream(file.path).pipe(stream);
+//   fs.createReadStream(file.path).pipe(stream);
 
-  console.log("uploading....");
+//   console.log("uploading....");
 
-  stream.on("error", (err) => {
-    console.error(err);
-    res.status(500).send("Something went wrong");
-  });
+//   stream.on("error", (err) => {
+//     console.error(err);
+//     res.status(500).send("Something went wrong");
+//   });
 
-  stream.on("finish", () => {
-    res.status(201).send("File uploaded successfully");
-  });
-});
+//   stream.on("finish", () => {
+//     res.status(201).send("File uploaded successfully");
+//   });
+// });
 
-module.exports = { uploadRouter };
+// module.exports = { uploadRouter };
